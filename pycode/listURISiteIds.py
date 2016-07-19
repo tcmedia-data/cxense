14427975c741affe610c7a6d8e6ce2d372191cd1
jamestweedietc <james.tweedie@tc.tc>
2016-07-19 13:57:35 -0400

    Create listURISiteIds.py

---
+++ pycode/listURISiteIds.py
@@ -0,0 +1,17 @@
+import cx
+import csv
+import json
+
+def listURLSiteIDs():
+    print 'SiteGroupName;SiteGroupId;SiteName;SiteId;SiteURL;DMPEnabled;Sentiment;Timezone'
+    listSiteGroups = json.loads(cx.execute(cx.getUrl('/site/group'),'' , cx.username, cx.secret)[2])
+    for siteGroup in listSiteGroups['siteGroups']:
+        sites = siteGroup['siteIds']
+        for siteid in sites:
+            siteDefinition = json.loads(cx.execute(cx.getUrl('/site'), '{"siteId":"' + siteid + '"}', cx.username, cx.secret)[2])
+            details = siteDefinition['sites']
+            for  site in details:
+                print siteGroup['name'] + ';"' + str(siteGroup['id']) + '";' + site['name'] + ';"' + str(site['id'])+ '";' + site['url'] + ';' + str(site['dmp']) + ';' + str(site['sentimentDetection']) + ';' + site['timeZone']
+
+if __name__ == "__main__":
+    listURLSiteIDs()
